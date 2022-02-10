39ca252189b505afa1540afc0c57f3d476dd103d
Camilo Guzman <camiloge_91@hotmail.com>
2022-02-10 12:15:05 -0500

    initial setup

---
+++ src/components/PokemonList/PokemonCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Grid, Image, Label, Icon } from 'semantic-ui-react';
+import { MAIN_COLOR, FAV_COLOR } from '../../utils/constants';
+import './styles.css';
+
+const PokemonCard = () => {
+  return (
+    <Grid.Column mobile={16} tablet={8} computer={4}>
+      <div className='PokemonCard'>
+        <Image centered src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png' alt='Pokemon front' />
+        <Icon name='favorite' color={FAV_COLOR} />
+        <p className='Pokemon-title'>Ditto</p>
+        <Label color={MAIN_COLOR}>Normal</Label>
+      </div>;
+    </Grid.Column>
+  );
+};
+
+export default PokemonCard;
---
+++ src/components/PokemonList/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import PokemonCard from './PokemonCard';
+
+const PokemonList = () => {
+  const pokemons = Array(20).fill('');
+
+  return (
+    <Grid className='PokemonList'>
+      {pokemons.map((pokemon) => (
+        <PokemonCard />
+      ))}
+    </Grid>
+  );
+};
+
+export default PokemonList;
