27d16c34e0dcf949f78d5ff37427331faa3de767
Camilo Guzman <camiloge_91@hotmail.com>
2022-02-17 09:17:39 -0500

    pokeapi_intro

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getPokemons } from '../../api/getPokemons';
 import PokemonList from '../../components/PokemonList';
 import Searcher from '../../components/Searcher';
 import './styles.css';
 
 function Home() {
+
+  useEffect(() => {
+    getPokemons().then((response) => {
+      console.log(response)
+    });
+  }, [])
+
   return (
     <div className='Home'>
       <Searcher />
