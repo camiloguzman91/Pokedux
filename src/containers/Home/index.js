28551af7083acf9c15c7c520f803ef034fcecee8
Camilo Guzman <camiloge_91@hotmail.com>
2022-02-23 10:50:53 -0500

    pre_async_requests

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,26 +1,29 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getPokemons } from '../../api/getPokemons';
-import PokemonList from '../../components/PokemonList';
 import Searcher from '../../components/Searcher';
-import { setPokemon } from '../../actions';
+import PokemonList from '../../components/PokemonList';
+import { getPokemons } from '../../api/getPokemons';
+import { setPokemons, setError } from '../../actions';
 import './styles.css';
 
 function Home() {
-
+  const pokemons = useSelector((state) => state.list);
   const dispatch = useDispatch();
-  const list = useSelector(state => state.list);
-  
+
   useEffect(() => {
-    getPokemons().then((res) => {
-      dispatch(setPokemon(res.results));
-    });
-  }, [])
+    getPokemons()
+      .then((res) => {
+        dispatch(setPokemons(res.results));
+      })
+      .catch((error) => {
+        dispatch(setError({ message: 'Ocurrió un error' , error}));
+      });
+  }, []);
 
   return (
     <div className='Home'>
       <Searcher />
-      <PokemonList pokemons={list} />
+      <PokemonList pokemons={pokemons} />
     </div>
   );
 }
